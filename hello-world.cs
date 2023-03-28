b1de967a08703e2768142bb8946009fc20c35931
Nolen Lawton <mnlawton@icloud.com>
2023-03-28 13:12:46 -0500

    finished base

--- hello-world.cs
+++ hello-world.cs
@@ -1,1 +1,66 @@
 ﻿Console.WriteLine("Hello, World!");
+
+//  Make a string
+string myString = "my string";
+
+Console.WriteLine(myString);
+
+//  Make a number (what kind? There are multiple!)
+int a = 8;
+double b = 8;
+decimal c = 8;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+//  Make a boolean
+bool isBoolean = true;
+
+Console.WriteLine(isBoolean);
+
+//  Make an if / else if / else block
+if (isBoolean) {
+    Console.WriteLine("this is true");
+} else {
+    Console.WriteLine("this is false");
+}
+
+//  Make a switch block
+switch(a) 
+{
+  case 1:
+    Console.WriteLine("a is 1");
+    break;
+  case 2:
+    Console.WriteLine("a is 2");
+    break;
+  default:
+    Console.WriteLine("default");
+    break;
+}
+
+//  Make an array of strings
+string[] arrayOfStrings = {"One", "Two", "Three"};
+
+Console.WriteLine("list of strings " + arrayOfStrings[0] + arrayOfStrings[1] + arrayOfStrings[2]);
+
+//  Make a for loop
+for (int i = 0; i < 3; i++){
+    //  Log each item
+    Console.WriteLine("for loop " + arrayOfStrings[i]);
+};
+
+//  Use a foreach loop
+foreach (string i in arrayOfStrings) 
+{
+  Console.WriteLine("foreach loop " + i);
+};
+
+//  Use a List instead of an array (HINT)
+List<string> listOfStrings = new List<string>();
+listOfStrings.Add("One");
+listOfStrings.Add("Two");
+listOfStrings.Add("Three");
+
+Console.WriteLine("list of strings " + listOfStrings[0] + listOfStrings[1] + listOfStrings[2]);
